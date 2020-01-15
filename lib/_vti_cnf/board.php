c0df4c1881f1d2ab3387a521d57fddac7116cfa7
BabisTsaga <57722419+BabisTsaga@users.noreply.github.com>
2020-01-15 20:40:06 +0200

    Egine apo ton it154618

---
+++ lib/_vti_cnf/board.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Jan 2020 20:19:27 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-IBJIM8T\\kwsta
+vti_modifiedby:SR|DESKTOP-IBJIM8T\\kwsta
+vti_timecreated:TR|13 Jan 2020 18:04:50 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|13 Jan 2020 18:04:50 -0000
+vti_cacheddtm:TX|13 Jan 2020 18:04:50 -0000
+vti_filesize:IR|5904
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
